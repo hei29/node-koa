e10c4d3579bd2478f24597b06b21a0387e7b455c
xytao <2431352054@qq.com>
2024-01-14 18:55:56 +0800

    删除本地logger信息，屏蔽本地文件

--- db/seq.js
+++ db/seq.js
@@ -14,5 +14,12 @@ const {Sequelize} = require('sequelize')
 // 方法二：使用连接 URI '使用数据库(mysql/postgres/...)://数据库账号:数据库密码@ip地址:端口/数据库名字' 
 const sequelize = new Sequelize('postgres://postgres:postgres@localhost:3336/test')
 
+// 测试连接
+// sequelize.authenticate().then(() => {
+//     console.log('连接成功')
+// }).catch(err => {
+//     console.log('连接失败', err)
+// })
+
 
 module.exports = sequelize
